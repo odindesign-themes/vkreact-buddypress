7da3b0d1d5d536bb014e47a89219ecb709f96bd4
odindesign-themes <71028280+odindesign-themes@users.noreply.github.com>
2020-10-22 14:00:31 -0300

    Adds nonce checks for all AJAX functions.

--- includes/ajax/vkreact-bp-ajax-activity.php
+++ includes/ajax/vkreact-bp-ajax-activity.php
@@ -9,6 +9,9 @@
  * Create a user reaction for an activity
  */
 function vkreact_bp_create_activity_user_reaction_ajax() {
+  // nonce check, dies early if the nonce cannot be verified
+  check_ajax_referer('vikinger_ajax');
+
   $result = vkreact_bp_create_activity_user_reaction($_POST['args']);
 
   header('Content-Type: application/json');
@@ -18,12 +21,14 @@ function vkreact_bp_create_activity_user_reaction_ajax() {
 }
 
 add_action('wp_ajax_vkreact_bp_create_activity_user_reaction_ajax', 'vkreact_bp_create_activity_user_reaction_ajax');
-add_action('wp_ajax_nopriv_vkreact_bp_create_activity_user_reaction_ajax', 'vkreact_bp_create_activity_user_reaction_ajax');
 
 /**
  * Delete a user reaction for an activity
  */
 function vkreact_bp_delete_activity_user_reaction_ajax() {
+  // nonce check, dies early if the nonce cannot be verified
+  check_ajax_referer('vikinger_ajax');
+  
   $result = vkreact_bp_delete_activity_user_reaction($_POST['args']);
 
   header('Content-Type: application/json');
@@ -33,6 +38,5 @@ function vkreact_bp_delete_activity_user_reaction_ajax() {
 }
 
 add_action('wp_ajax_vkreact_bp_delete_activity_user_reaction_ajax', 'vkreact_bp_delete_activity_user_reaction_ajax');
-add_action('wp_ajax_nopriv_vkreact_bp_delete_activity_user_reaction_ajax', 'vkreact_bp_delete_activity_user_reaction_ajax');
 
 ?>
